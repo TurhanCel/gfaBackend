d1f922eb807d41674c8f8cf9bbda43d1aa282586
TurhanCel <turhan.sagbas@student.odisee.be>
2025-03-08 20:09:02 +0100

    ??

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -21,8 +21,8 @@ const getMailTransporter = () => {
     } else if (provider === "hostinger") {
         config = {
             host: "smtp.hostinger.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
